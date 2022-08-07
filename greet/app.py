c6f4c38260775a532f534ab87f0513e1d483f7fa
ShanShan Li <shanshanli002@gmail.com>
2022-08-07 18:09:28 -0500

    updating greet routes

--- greet/app.py
+++ greet/app.py
@@ -2,3 +2,15 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
+
+@app.route('/welcome', methods=["GET"])
+def welcome_msg():
+    return "welcome"
+
+@app.route('/welcome/home', methods=["GET"])
+def welcome_home():
+    return "welcome home"
+
+@app.route('/welcome/back', methods=["GET"])
+def welcome_back():
+    return "welcome back"
